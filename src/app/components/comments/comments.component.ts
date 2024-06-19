2e027eb7cabba3cc92fcbaa01da38197eef66919
JJerome-NM <jjerome010@gmail.com>
2024-06-19 22:05:18 +0300

    [LAB] Added comments component

--- src/app/components/comments/comments.component.ts
+++ src/app/components/comments/comments.component.ts
@@ -2,6 +2,7 @@ import {Component, Input, OnInit} from '@angular/core';
 import {MatSnackBar} from "@angular/material/snack-bar";
 import {FormControl, FormGroup} from "@angular/forms";
 import {IAssignmentComment, ICommentBase, ISubmissionComment} from "../../models/IComment";
+import {IUser} from "../../models/IUser";
 
 interface IAddCommentForm {
   message: FormControl<string>
@@ -16,6 +17,12 @@ export class CommentsComponent implements OnInit {
   @Input() comments: ICommentBase[] | IAssignmentComment[] | ISubmissionComment[] = [];
   @Input() editable: boolean = false;
 
+  protected user: IUser = {
+    id: '1',
+    name: 'JJerome',
+    email: 'jjerome@gmail.com',
+    role: 'USER'
+  }
   private commenetType: "BASE" | "ASSIGNMENT" | "SUBMISSION" = "BASE"
 
   protected addCommentForm: FormGroup<IAddCommentForm> = new FormGroup<IAddCommentForm>(<IAddCommentForm>{
@@ -60,5 +67,34 @@ export class CommentsComponent implements OnInit {
         message: this.addCommentForm.get('message')?.value || ""
       });
     }
+  }
+
+  protected getUserColor(userId: string): string {
+    const hashCode = (str: string): number => {
+      let hash = 0;
+      for (let i = 0; i < str.length; i++) {
+        hash = str.charCodeAt(i) + ((hash << 5) - hash);
+      }
+      return hash;
+    };
+
+    const randomColor = () => {
+      const hash = Math.abs(hashCode(userId));
+      let r = (hash & 0xFF0000) >> 16;
+      let g = (hash & 0x00FF00) >> 8;
+      let b = hash & 0x0000FF;
+
+      const minBrightness = 128;
+      const brightness = (r * 299 + g * 587 + b * 114) / 1000;
+      if (brightness > minBrightness) {
+        r = Math.floor(r * minBrightness / brightness);
+        g = Math.floor(g * minBrightness / brightness);
+        b = Math.floor(b * minBrightness / brightness);
+      }
+
+      return '#' + ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+    };
+
+    return randomColor();
   }
 }
